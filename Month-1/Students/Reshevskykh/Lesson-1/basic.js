ee8ac7a0102a0a0bd4fd3fe3968e1fa8733fd41d
MarynaResh <marynakrupka@gmail.com>
2022-08-15 23:57:04 +0300

    Test

--- Month-1/Students/Reshevskykh/Lesson-1/basic.js
+++ Month-1/Students/Reshevskykh/Lesson-1/basic.js
@@ -1,10 +1,7 @@
-/*
-Устанавливаем cypress:
-npm install cypress
-*/
+// Home task
 
-/*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
+let uid = 'B9D0073A3865C6EBE053050310AC3559';
+let amount = 12.20;
+const clientId = '9tsFyTh9';
+const commission = 1.12:
 
