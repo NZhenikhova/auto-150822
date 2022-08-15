0b5ea43e479f04a8191d09eeedbbd7e28a9643e6
MarynaResh <marynakrupka@gmail.com>
2022-08-16 01:13:01 +0300

    Added home task #1

--- Month-1/Students/Reshevskykh/Lesson-1/basic.js
+++ Month-1/Students/Reshevskykh/Lesson-1/basic.js
@@ -2,6 +2,22 @@
 
 let uid = 'B9D0073A3865C6EBE053050310AC3559';
 let amount = 12.20;
+let uid2 = 'B9D0073A3865C6EBE053050310AC3559';
 const clientId = '9tsFyTh9';
-const commission = 1.12:
+const commission = 1.12;
+const commission2 = '1.12'; 
+console.log(uid == uid2);
+console.log(uid === uid2);
+console.log(uid != commission);
+console.log(uid !== commission);
+console.log(commission2 != commission);
+console.log(commission2 > commission2);
+console.log(commission2 >= commission);
+console.log(commission2 < amount);
+console.log(commission <= amount);
+
+
+
+
+
 
