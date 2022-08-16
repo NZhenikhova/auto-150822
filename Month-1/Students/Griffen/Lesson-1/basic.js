165e22d74cf93707fd59e250f309df8fb686e27f
Griffen <marina.griffen14@gmail.com>
2022-08-16 12:57:50 +0300

    home task

--- Month-1/Students/Griffen/Lesson-1/basic.js
+++ Month-1/Students/Griffen/Lesson-1/basic.js
@@ -1,10 +1,20 @@
-/*
-Устанавливаем cypress:
-npm install cypress
-*/
+// Home tasks
+let dogName = 'Amour';
+let dogAge = 1;
+let catName = 'Mike';
+let catName2 = 'Mike';
+let catAge = 2;
+let catPaws = '4';
+const dogPaws = 5
 
-/*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
 
+console.log (catAge > dogAge); // true
+console.log (catPaws < catAge); // false
+console.log (catPaws >= dogPaws); // false
+console.log (catPaws <= dogPaws); // true
+console.log (catName != dogName); // true
+console.log (catName !== catName2); // false
+console.log (catPaws == dogPaws); // false
+console.log (catAge === dogAge); // false
+
+console.log (typeof catPaws)
