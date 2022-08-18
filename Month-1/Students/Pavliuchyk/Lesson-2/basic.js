562479c8d829881adb69aff4aed947d0a07a9cf7
Olga-Pavliuchyk <olga.pavliuchyk@gmail.com>
2022-08-18 19:18:04 +0300

    task

--- Month-1/Students/Pavliuchyk/Lesson-2/basic.js
+++ Month-1/Students/Pavliuchyk/Lesson-2/basic.js
@@ -1,10 +1,11 @@
-/*
-Устанавливаем cypress:
-npm install cypress
-*/
 
-/*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
+let index = 10;
 
+if(index > 0 && index < 5){
+    console.log('Первый блок')
+} else if(index > 5 || index < 10) 
+{
+    console.log('Второй блок');
+} else if (index !=10) {
+    console.log('Третий блок');
+};
