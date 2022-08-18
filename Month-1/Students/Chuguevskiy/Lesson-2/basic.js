730fb96d4eee97eb6ff42b5f10cd8314f6560575
MrXof <verulanedanill@gmail.co>
2022-08-18 19:15:13 +0300

    test 2

--- Month-1/Students/Chuguevskiy/Lesson-2/basic.js
+++ Month-1/Students/Chuguevskiy/Lesson-2/basic.js
@@ -1,10 +1,57 @@
-/*
-Устанавливаем cypress:
-npm install cypress
-*/
+
+
 
 /*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
+if ( x < y ){
+    console.log("one");
+}else if(x == 11){    
+    console.log("two");
+}else{
+    console.log("three");
+};
+
+*   && - и
+*   || - или 
+*   !true - не ( bollean )
+*
+*
+*
+*
+
+
+
+let index = 7;
+if(index <5){
+    console.log("Wow");
+}else if (index > 5 && index <10){
+    console.log("hard");
+}else{
+    console.log("Bro");
+};
+
+
+const num = 40;
+const result = num <10 || num > 30;
+console.log(result);
+
+
+const pets = true;
+if(pets == !true){
+    console.log("привет");
+}else{
+    console.log("нет");
+}
 */
+
+
+let x = 10;
+time = 9;
+
+if(x == time && x > time ){
+    console.log("Первый блок");
+}else if (x < time || x == time){
+console.log("Второй блок");
+}else{
+    console.log("Третий блок");
+};
 
