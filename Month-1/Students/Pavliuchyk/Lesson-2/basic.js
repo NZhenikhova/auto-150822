d44131386f0cb5517b28471ddae44a9640de026b
Olga-Pavliuchyk <olga.pavliuchyk@gmail.com>
2022-08-18 20:16:45 +0300

    push switch

--- Month-1/Students/Pavliuchyk/Lesson-2/basic.js
+++ Month-1/Students/Pavliuchyk/Lesson-2/basic.js
@@ -14,3 +14,19 @@ const dog = 5;
 const cat = '5';
 
 dog === cat ? (console.log("привет")) : (console.log('пока'));
+
+let num = 2;
+let result;
+
+switch (num) {
+    case num==1: result='зима' ; console.log(result)
+    break;
+    case num==2: result='весна'; console.log(result)
+    break;
+    case num==3: result='лето'; console.log(result);
+    break;  
+    case num==4: result='осень'; console.log(result);
+    break;  
+    default: 'not found'
+break;
+}
