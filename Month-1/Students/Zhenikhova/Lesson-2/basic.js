6917d654233478a421e1923c034f459b3d43f613
Your Name <you@example.com>
2022-08-18 09:41:03 -0700

    natasha

--- Month-1/Students/Zhenikhova/Lesson-2/basic.js
+++ Month-1/Students/Zhenikhova/Lesson-2/basic.js
@@ -1,10 +1,12 @@
-/*
-Устанавливаем cypress:
-npm install cypress
-*/
 
-/*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
 
+const dog = 5;
+const cat ='5';
+
+if(dog === cat){
+    console.log(Hi);
+} else{
+    console.log('by');
+}
+
+dog  === cat ? console.log(true) : console.log(false);
