8e99f7178982477da7cea82e49fb369270940692
DariaLeonova <leonovadaria18@gmail.com>
2022-08-18 20:17:19 +0300

    Leonov's practical task!!!

--- Month-1/Students/Leonova/Lesson-2/basic.js
+++ Month-1/Students/Leonova/Lesson-2/basic.js
@@ -1,10 +1,47 @@
-/*
-Устанавливаем cypress:
-npm install cypress
-*/
+// Первое практическое задание
 
-/*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
+let member = 10;
+if (member < 10) {
+    console.log ('1 блок');
+ } else if (member == 10) {
+    console.log ('2 блок');
+} else{
+    console.log ('3 блок');
+};
 
+let member1 = 2;
+if (member1 < 10) {
+    console.log ('1 блок');
+ } else if (member1 == 10) {
+    console.log ('2 блок');
+} else{
+    console.log ('3 блок');
+};
+
+let member2 = 12;
+if (member2 < 10) {
+    console.log ('1 блок');
+ } else if (member2 == 10) {
+    console.log ('2 блок');
+} else{
+    console.log ('3 блок');
+};
+
+// Второе практическое задание Переменная num может принимать 4 значения
+
+const num = '1';
+switch (num) {
+    case '1': console.log ('зима');
+    break;
+
+    case '2': console.log ('весна');
+    break;
+
+    case '3': console.log ('лето');
+    break;
+
+    case '4': console.log ('осень');
+    break;
+
+
+}
