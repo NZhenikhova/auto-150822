e1a9d43ab4f490650e89df65e36ad0cce576efd2
MaxmARK107 <maximskachenko@gmail.com>
2022-08-18 19:33:45 +0300

    Max1

--- Month-1/Students/Skachenko/Lesson-2/basic.js
+++ Month-1/Students/Skachenko/Lesson-2/basic.js
@@ -7,4 +7,16 @@ npm install cypress
 Устанавливаем проверочные тесты и запускаем cypress:
 npx cypress open
 */
-
+/*
+let time = 10;
+if (time > 10 && time < 50)(
+    console.log('Первый блок');
+)else( if (time > 50 || time < 100)(
+    console.log('Второй блок');
+))else( if (time !== 0)(
+    console.log('Третий блок');
+));
+*/
+const dog = 5;
+const cat = '5';
+dog === cat ? console.log('верно') : console.log('не верно');
