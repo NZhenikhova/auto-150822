4a84cd187d2007c397fe9762b9700a5a061701f0
Kirill <kyrylogolovkov@gmail.com>
2022-08-18 19:34:41 +0300

    111

--- Month-1/Students/Golovkov/Lesson-2/basic.js
+++ Month-1/Students/Golovkov/Lesson-2/basic.js
@@ -1,10 +1,7 @@
-/*
-Устанавливаем cypress:
-npm install cypress
-*/
+let name = 65
+let name1 = 70
 
-/*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
+if(name1 > name && name1>30){console.log('готово')}
+else(console.log(name1))
 
+name1===name ? console.log(name1): console.log(name);
