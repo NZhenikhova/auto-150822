51b989d6a940f1471e36b4fd73c5d7c37f2c630b
tpiddubna <tpiddubna@student.ucode.world>
2022-08-22 19:38:26 +0300

    class_work

--- Month-1/Students/Piddubna/Lesson-3/basic.js
+++ Month-1/Students/Piddubna/Lesson-3/basic.js
@@ -1,10 +1,56 @@
-/*
-Устанавливаем cypress:
-npm install cypress
-*/
+//Class. Functions
+// nameFunction();
 
-/*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
+// function.nameFunction() {
+//     console.log('Function Declared');
+// }
+// let fruit = 'apple';
+// let number = 13;
 
+// function showCountApples(){
+//     console.log('I have ${number} of ${fruit}');
+// }
+// showCountApples();
+
+// let name = 'Tania';
+
+// function myName(){
+//     console.log(`Hello my name is ${name}`);
+// }
+// myName();
+
+//Function expression
+// const funcExpress = function(){
+//     console.log('function expression');
+// }
+// funcExpress();
+
+// //Declarative funtions
+// function testFunction() {
+//     console.log('Thats declerative function');
+// }
+// testFunction();
+//Parametrs and arguments for function
+// function findElement (parameterName){
+//     console.log(`The page ${parameterName} can not be found`);
+
+// }; 
+
+// let page = 'Home';
+// findElement(page);
+
+// let page = 'Home Page';
+// function findElementbyParams(parameterName, parameterName1, parameterName2) {
+//     console.log(`On the page ${parameterName}, element ${parameterName1} of the class ${parameterName2}`);
+// };
+// findElementbyParams(page, 'Work page', 'Terms page');
+
+let myNumb = 10;
+function Numb(){
+   if (myNumb == 18 || myNumb > 18){
+    console.log('true');
+   }else{
+    console.log('false');
+   }
+};
+Numb();
