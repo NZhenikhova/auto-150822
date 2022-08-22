72398343c7ee83914cb22e3f26386113d40abb8b
Olga-Pavliuchyk <olga.pavliuchyk@gmail.com>
2022-08-22 19:12:44 +0300

    lesson

--- Month-1/Students/Pavliuchyk/Lesson-3/basic.js
+++ Month-1/Students/Pavliuchyk/Lesson-3/basic.js
@@ -1,10 +1,22 @@
-/*
-Устанавливаем cypress:
-npm install cypress
-*/
+// function declaration
 
-/*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
+nameFunction();
+function nameFunction() {
+    console.log ('Функция вызвалась');
+    }
 
+let name1 = 'Olga';
+let surname = 'Pavliuchyk';
+
+function showNameSurname() {
+    console.log (`My name is ${name1} ${surname}`);
+}
+showNameSurname();
+
+
+// function expression
+
+const functions = function(){
+    console.log(`function expression`);
+}
+functions();
