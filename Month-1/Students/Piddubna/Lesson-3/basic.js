b429b56eb06019376d510011bc71b9d2b425333e
tpiddubna <tpiddubna@student.ucode.world>
2022-08-22 20:39:10 +0300

    classwork_lesson3_Piddubna

--- Month-1/Students/Piddubna/Lesson-3/basic.js
+++ Month-1/Students/Piddubna/Lesson-3/basic.js
@@ -45,6 +45,7 @@
 // };
 // findElementbyParams(page, 'Work page', 'Terms page');
 
+//Solution 1
 let myNumb = 10;
 function Numb(){
    if (myNumb == 18 || myNumb > 18){
@@ -54,3 +55,63 @@ function Numb(){
    }
 };
 Numb();
+
+// let func1 = function(num1, num2){
+//     let result = num1*num2;
+//     console.log('Thats expression function', result);
+// };
+// func1(1,2);
+
+//Arrow function
+// let funcAr = (num1, num2) => {
+//     let result = num1*num2;
+//     console.log('Thats arrow function', result);
+// };
+// funcAr(3,4);
+
+//Return
+// function ret (value, value1, value2) {
+//     const newRet = value, value1, value2;
+//     return res;
+// };
+// let value = 10;
+// let value1 = 12;
+// let value2 = 123;
+
+// const result = test(value,value1,value2);
+// console.log(result);
+
+// function summary(a,b) {
+//     return a+b;
+// };
+// let result = summary(10,12);
+// console.log('Summary of numbers', result);
+
+// console.log(summary(10,12));
+
+// //function as result
+// const fullResult = summary(summary(1,3),summary(5,6));
+// console.log('Full result', fullResult);
+
+//Solution 2
+function equation(one, two){
+    return one+two;
+};
+let newSum = equation(5,7);
+console.log('This is summary of',newSum);
+
+
+//help for hometask
+function isStrongPassword(password) {
+const length = password.length; 
+
+return length > 8 && length < 20
+};
+
+const one = isStrongPassword('password');
+const two = isStrongPassword('crazylastochka');
+const three = isStrongPassword('randomcrapgoesherecantfixit');
+
+console.log('Check whether password is strong',one);
+console.log('Check whether password is strong',two);
+console.log('Check whether password is strong',three);
