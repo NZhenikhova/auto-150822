5d6b238344ddef585c3edc2da3e191139b7002d7
Olga-Pavliuchyk <olga.pavliuchyk@gmail.com>
2022-08-22 19:34:15 +0300

    task

--- Month-1/Students/Pavliuchyk/Lesson-3/basic.js
+++ Month-1/Students/Pavliuchyk/Lesson-3/basic.js
@@ -20,3 +20,26 @@ const functions = function(){
     console.log(`function expression`);
 }
 functions();
+
+//параметры и аргументы функций
+
+function findElement (parametrName){
+    console.log(`На странице ${parametrName} элемент не обнаружен`);
+}
+
+let page = 'Home';
+findElement(page);
+
+function findElement2 (parametrName, parametrName2, parametrName3){
+    console.log(`На странице ${parametrName} элемент ${parametrName2} класса ${parametrName3} не обнаружен`);
+};
+
+let page2 = 'Home';
+findElement2(page2, 'registration page', 'work');
+
+
+function proverka (number1){
+    number1 > 18 ? console.log('true') : console.log ('false');
+    }
+    let number1 = 19;
+    proverka(number1);
