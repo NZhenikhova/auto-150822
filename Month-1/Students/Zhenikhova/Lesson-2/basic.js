7896f415d19449311720fced9355874edc4f5ed0
Your Name <you@example.com>
2022-08-21 21:18:30 -0700

    homework 2

--- Month-1/Students/Zhenikhova/Lesson-2/basic.js
+++ Month-1/Students/Zhenikhova/Lesson-2/basic.js
@@ -1,12 +1,59 @@
+let a = 'test'
+if( a ==='test'){    
+    console.log('Верно');
+} else {
+    console.log ('Неверно');
+};
 
 
-const dog = 5;
-const cat ='5';
 
-if(dog === cat){
-    console.log(Hi);
-} else{
-    console.log('by');
+let customerAge = 60 
+if(customerAge === 18){
+    console.log('Возраст совпал');
+} else if (customerAge > 18 && customerAge < 30){
+    console.log ('Категория - 1');
+} else if (customerAge >30){
+   console.log ('"Возраст пользователя" подходящий для приема на работу');
+};
+
+
+let b = 3;
+let c = 5;
+if(b <= 1 && c >= 3){
+    console.log(b+c);
+} else {
+    console.log(b-c)};
+
+
+let month = 4;
+    if (month >= 12 && month <= 2){
+        console.log ('зима');
+    } else if (month >= 3 && month <= 5){
+        console.log ('весна');
+    } else if (month >= 6 && month <= 8){
+        console.log ('лето');
+    } else if (month >= 9 && month <=11){
+        console.log ('осень');
+    };
+
+
+
+    let browser = 'Edge';
+    if (browser == 'Edge'){
+        console.log ('You've got the Edge!');
+    } switch ('browser') {
+        Case == 'Chrome';
+        break;
+        Case =='Firefox'; 
+        break;
+        Case ==  'Safari';
+        break;
+        Case =  'Opera';
+        break;
+        console.log ('O'key we support these browser too');
+    };
+    else {
+        console.log ('We hope that this page looks ok!');
 }
+    
 
-dog  === cat ? console.log(true) : console.log(false);
