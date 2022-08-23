a2c46bbb8345332bcf00afa2b59c6afd5b790379
MaxmARK107 <maximskachenko@gmail.com>
2022-08-23 20:04:20 +0300

    Max3

--- Month-1/Students/Skachenko/Lesson-3/basic.js
+++ Month-1/Students/Skachenko/Lesson-3/basic.js
@@ -34,3 +34,51 @@ function summ (a, b) {
 };
 let result = summ (3, 6);
 console.log(result);
+
+
+function isStrongPassword (password) {
+    const length = password. length;
+
+    return length > 8 && length < 20;
+};
+
+const ini = isStrongPassword ('luhvbafvbljvsgnsdhnsdynsrnbhadfb');
+
+console.log('Check word password is strong ', ini);
+
+
+/// Задание 1/// 
+/*Напишем стрелочную функцию, проверяющую квартиру.
+Она принимает два аргумента: площадь (число) и название улицы (строку):
+площадь должна быть больше либо равна 100 кв.м ИЛИ больше либо равна 80 кв.м И
+улица должна быть равна “School-street”*/ 
+console.log('/// Задание 1///');
+
+const flat = (square, street) => {
+    square = square >= 80 ? 'true' : 'false';
+    street = street == 'School-street' ? 'true' : 'false';
+return square, street
+};
+console.log(flat(101,'School-street'));
+
+/// Задание 2/// 
+/*Реализуйте функцию, который проверяет формат указанного телефона.
+Если телефон начинается с +, значит это международный формат.*/ 
+console.log('/// Задание 2///');
+
+function phoneNumber (a) {
+    firstSymbol = a[0] == '+' ? 'Международный формат' : 'Не международный формат'
+    return firstSymbol;
+};
+console.log(phoneNumber('+380991234567'));
+
+/// Задание 3/// 
+/*Напишите стрелочную функцию которая проверит тип данных аргумента на ‘string’ 
+и выведет в консоль true.*/ 
+console.log('/// Задание 3///');
+
+const typeCheck = (b) => {
+    result = typeof(b) == 'string' ? 'true' : 'false'
+    return result;
+};
+console.log(typeCheck('+380991234567'));
