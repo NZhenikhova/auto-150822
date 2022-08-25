bfd277e7603f8903b9f17dc09f4afa3adf2eacb2
MrXof <verulanedanill@gmail.co>
2022-08-25 20:31:33 +0300

    test

--- Month-1/Students/Chuguevskiy/Lesson-4/basic.js
+++ Month-1/Students/Chuguevskiy/Lesson-4/basic.js
@@ -22,25 +22,41 @@ console.log(myPets.length,myPets[3]);
 
 const allCars = ["BMW","Mercedes","Volvo","Volkswagen"];
 
-///console.log(allCars.push("Infiniti")); // add
+///console.log(allCars.push("Infiniti"));                                                     // add
 //console.log(allCars);
 //console.log(allCars.pop()); //delete 
 
-//allCars.shift();   // delete first element[0] 
+//allCars.shift();                                                                        // delete first element[0] 
 //console.log(allCars);
 
-allCars.splice(2,2,"Audi","Seat");  // delete  [0 - 1] 
+allCars.splice(2,2,"Audi","Seat");                                                     // delete  [0 - 1] 
 console.log(allCars);   
 
 const pets = ["Marfa","Kyzya","Iko"];  
-console.log(pets.includes("Marfa"));    // includes - проверка на наличие в массиве
+console.log(pets.includes("Marfa"));                                               // includes - проверка на наличие в массиве
 
-pets.unshift("Kesha");      // unshift - добавляет елемент в начало массива
+pets.unshift("Kesha");                                                           // unshift - добавляет елемент в начало массива
 console.log(pets);
 
-*/
 
-allFavoriteCars = ['BMW', 'Mercedes', 'Volvo', 'Volkswagen'];
+
+const allFavoriteCars = ['BMW', 'Mercedes', 'Volvo', 'Volkswagen'];
 allFavoriteCars.push("Infiniti");
 allFavoriteCars.unshift("Seat");
 console.log(allFavoriteCars);
+
+
+/// Цыкл 
+
+for (let i=0;i<=10;i++){
+    console.log(i);
+};
+
+*/
+
+
+
+for(let i =0; i <= 10 ; i++){
+    console.log(i);
+};
+
