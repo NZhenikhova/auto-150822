3cc1c6ce71ddf4abbea3eea4137da84d03d2474c
MaxmARK107 <maximskachenko@gmail.com>
2022-08-29 20:24:41 +0300

    Max5

--- Month-1/Students/Skachenko/Lesson-5/basic.js
+++ Month-1/Students/Skachenko/Lesson-5/basic.js
@@ -7,4 +7,78 @@ npm install cypress
 Устанавливаем проверочные тесты и запускаем cypress:
 npx cypress open
 */
+/*
 
+const userName = {
+    name: 'Bob',
+    age: '18',
+    flat: 'yes',
+    'profession': true,
+    adress: {
+        country: 'Germany',
+        city: 'Berlin',
+        street: 'Leipzig strasse'
+    }
+};
+/*
+console.log(userName);
+console.log(userName.age);
+
+//Добавление
+userName.profession = 'true';
+console.log(userName);
+
+//Удаление
+delete userName.profession;
+console.log(userName);
+
+//Создание нового объекта
+
+let object = new Object ();
+let object2 {};
+
+
+
+console.log(userName);
+console.log(userName.adress);
+console.log(userName.adress.city);
+
+for (let propertyName in userName) {
+    console.log(propertyName, userName[propertyName]);
+};
+
+for (let propertyName in userName) {
+    if (propertyName == 'age') {
+        console.log(userName.age);
+    };
+    
+};
+
+
+const human = {
+    name: 'Bob',
+    surname: 18,
+    age: 'before',
+    color: function () {
+        return this.surname;
+    }
+};
+const man = human.color();
+console.log(man);
+*/
+
+const Jedi = {
+    name: 'Yoda',
+    saber: 'green',
+    age: 900,
+    power: 'Force',
+    ability: function () {
+        return this.power
+    } 
+};
+
+for (let param in Jedi){
+    if (param == age) {
+        return Jedi.ability()
+    }
+};
