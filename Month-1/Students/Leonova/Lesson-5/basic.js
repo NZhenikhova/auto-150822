53e958b54dfee51461146053cc45fef8f852ab96
DariaLeonova <leonovadaria18@gmail.com>
2022-08-31 20:12:38 +0300

    Homework Lesson 5

--- Month-1/Students/Leonova/Lesson-5/basic.js
+++ Month-1/Students/Leonova/Lesson-5/basic.js
@@ -1,10 +1,43 @@
-/*
-Устанавливаем cypress:
-npm install cypress
-*/
+/* Задание 5 Задача на функции:
+Создайте два объекта. 
+В каждом из них есть функция. 
+Во втором объекте функция вызывает функцию первого объекта. 
+Далее в первой функции первого объекта мы проверяем есть ли “ключ - uid” у  второго объекта, 
+если есть то создаем новый  пустой объект к которому присваиваем второй объект, н
+овому объекту добавляем “ключ - changes, значение - true” и выводим в консоль новый объект. 
+(ожидаем объект с новым ключ- значением)*/
 
-/*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
+
+const citiesName = {
+    name : 'Dnipro',
+    age : '18',
+    square : '2222222',
+    color : function(){
+        if (countriesName.name && countriesName.age && countriesName.square && countriesName.color != null){                              /*“ключ - uid*/
+            const villageName = countriesName;
+            villageName.changes = 'true';
+            console.log(villageName);
+        };
+    }
+    };
+
+const countriesName = {
+    name : 'Ukraine',
+    age : '31',
+    square  : '6666666',  
+    color : function(){
+        let color = citiesName.color();
+      }
+}; countriesName.color();
+
+ 
+
+
+
+
+
+
+
+
+
 
