1b75b6ed8fc19d99bf7edfbb04cb16382160c5c7
MrXof <verulanedanill@gmail.co>
2022-09-06 11:15:17 +0300

    home

--- Month-1/Students/Chuguevskiy/Lesson-7/basic.js
+++ Month-1/Students/Chuguevskiy/Lesson-7/basic.js
@@ -1,10 +1,42 @@
-/*
-Устанавливаем cypress:
-npm install cypress
-*/
+// CSS 
+/*    teg[class=name]    */
+
+//form[data-form="register"] [type="submit"]
 
 /*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
+        Home Task
 
+        //mail
+CSS:
+#your-email22 
+
+        //Subject
+#your-subject2
+
+        //Description:
+
+#your-message002
+
+
+        //Deposit or Withdrawal
+
+#your-deposit
+
+        //Date of Deposit/Withdrawal
+
+#your-date2
+
+    
+        //Status of Transaction
+
+#your-status
+
+        //Provide blockchain explorer URL or transaction ID
+
+#your-provide
+
+        //Beaxy Transaction ID
+
+#your-id
+
+*/
