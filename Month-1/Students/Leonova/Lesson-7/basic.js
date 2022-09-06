6404050d9c3f9275e6277ab304f1f91d89b59f7a
DariaLeonova <leonovadaria18@gmail.com>
2022-09-06 15:04:14 +0300

    Home task 7 Leonova Daria

--- Month-1/Students/Leonova/Lesson-7/basic.js
+++ Month-1/Students/Leonova/Lesson-7/basic.js
@@ -1,10 +1,27 @@
 /*
-Устанавливаем cypress:
-npm install cypress
+Home task 7:
+Составьте css селекторы для формы “Deposits and Withdrawals” на странице суппорт
 */
 
-/*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
+/*CSS
+Your email address: #your-email22
 
+Subject: #your-subject2
+
+Description: #your-message002
+
+Deposit or Withdrawal: #your-deposit
+
+Date of Deposit/Withdrawal:#your-date2
+
+Status of Transaction:#your-status
+
+
+Provide blockchain explorer URL or transaction ID: #your-provide
+
+
+Beaxy Transaction ID: #your-id
+
+SUBMIT button: #button-16351548224132
+
+*/
