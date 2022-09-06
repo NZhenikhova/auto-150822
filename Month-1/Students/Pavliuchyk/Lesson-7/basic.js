c4dee2f9578baa998a7fe9038b8b4fb1f9b966e2
Olga-Pavliuchyk <olga.pavliuchyk@gmail.com>
2022-09-06 22:07:06 +0300

    hometask7

--- Month-1/Students/Pavliuchyk/Lesson-7/basic.js
+++ Month-1/Students/Pavliuchyk/Lesson-7/basic.js
@@ -1,10 +1,49 @@
-/*
-Устанавливаем cypress:
-npm install cypress
-*/
+//      tagname[attribute=value]
+//  input(тег)#(айди).класс 
+// type=['']
+//   form[data-form='register'] input[type='email]
+// div form через пробел (обращение 'на уровень' ниже)
 
-/*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
+#popUp-register-email
+#_pass
+#popUp-register-phone._valid-input
+#popUp-register-promo
+form[data-form="register"] [type="submit"]
+[data-form="register"] button
 
+/* Home task:
+Составьте css селекторы для формы “Deposits and Withdrawals” 
+на странице суппорт https://www.beaxy.com/help/support/
+
+Your email address:
+#your-email22
+input[name='your-email22'] или [name='your-email22']
+
+
+Subject
+#your-subject2
+input[name='your-subject2'] или [name='your-subject2']
+
+Description:
+#your-message002
+input[name='your-message002'] или [name='your-message002']
+
+Deposit or Withdrawal:
+#your-deposit
+select[name='your-deposit']
+
+Date of Deposit/Withdrawal
+#your-date2
+input[name='your-date2'] или [name='your-deposit']
+
+Status of Transaction
+#your-status    
+select[name='your-status'] или [name='your-status'] 
+
+Provide blockchain explorer URL or transaction ID
+#your-provide
+input[name='your-provide'] или [name='your-provide']   
+
+Beaxy Transaction ID
+#your-id
+input[name='your-id'] или [name='your-id'] */
