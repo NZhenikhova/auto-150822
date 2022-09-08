080dcc715f1f25ba6207727d9fa5f2a8528e9d34
Griffen <marina.griffen14@gmail.com>
2022-09-08 14:29:43 +0300

    hw 7 Griffen

--- Month-1/Students/Griffen/Lesson-7/basic.js
+++ Month-1/Students/Griffen/Lesson-7/basic.js
@@ -1,10 +1,54 @@
-/*
-Устанавливаем cypress:
-npm install cypress
+// CSS локаторы
+
+/*  tagname[attribute=value] */
+
+/* 
+Основные виды селекторов
+
+1. div - элементы с таким тегом
+2. #id - элемент с данным id
+3. .class - элементы с таким классом
+4. [name="value"] - атрибут и его знчение 
 */
 
-/*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
+
+
+/* div #_pass._valid-input  */
+
+/* form[data-form="register"][type="submit"] -- для кнопки  Sign in
+
+// Псевдоклассы
+
+/* .exchange-table-body [data-pair-name= "BXYBTC"] :first-child
+
+// [attribute^ = value]
+
+
+
+
+
+
+// Homework
+
+.select-css [value="2"]  -- выпадающий список выбора категории
+
+#your-email22 -- Your email address
+
+[name="your-subject2"] -- Subject
+
+.form-control[name="your-message002"] -- Description
+
+[name="your-deposit"] -- нажитие на выбор опции Deposit or Withdrawal:
+
+select[name=your-deposit] [value="Deposit"] -- выбор опции
+
+#your-date2 -- Date of Deposit
+
+[name="your-status"] > option[value="Error"] -- Status of Transaction
+
+[name="your-provide"] -- Provide blockchain explorer URL or transaction ID
+
+.form-control[name="your-id"] - Transaction ID
+
+#button-16351548224132 - Submit button
 
