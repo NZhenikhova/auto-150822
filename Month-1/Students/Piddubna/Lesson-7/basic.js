763ab12ef905c1994e2c00985ad02ce26ed89d9b
Tati <tati@MBP-admin.lan>
2022-09-12 14:24:29 +0200

    hometask updated(2 tasks)

--- Month-1/Students/Piddubna/Lesson-7/basic.js
+++ Month-1/Students/Piddubna/Lesson-7/basic.js
@@ -7,6 +7,30 @@
 // [class*='modal']
 // [name$='ID']
 
+////a[@href='https://www.beaxy.com/author/james-messi/']/preceding-sibling::p
+//a[@href='https://www.beaxy.com/author/james-messi/']/ancestor::article. - родитель родителя
+//div[@class='item-body__info']/child::a
+// ul:first-of-type
+//  :last-of-type
+//  :only-of-type
+//  :nth-of-type
+//  :nth-last-of-type
+
+
+
+/*
+1. :first-child – первый потомок своего родителя.
+
+2. :last-child – последний потомок своего родителя.
+
+3. :only-child – единственный потомок своего родителя, соседних элементов нет.
+
+4.:nth-child(a) – потомок номер a своего родителя, например :nth-child(2) – второй потомок. Нумерация начинается с 1.
+
+5. :nth-child(an+b) – расширение предыдущего селектора через указание номера потомка формулой, где a,b – константы, а под n подразумевается любое целое число.
+$$('[data-hook="colorGroup"] li label input')[3].click()
+*/
+
 //Hometask
 $x("input[id='your-email22']")
 $x("input[id='your-subject2']")
@@ -15,3 +39,5 @@ $x("select[id='your-deposit']")
 $x("select[id='your-status']")
 $x("input[id='your-provide']")
 $x("input[id='your-id']")
+
+
