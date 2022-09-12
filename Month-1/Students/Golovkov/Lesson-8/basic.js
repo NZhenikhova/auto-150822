373ba14668ec4686875d6e606d1222929ff07d72
Kirill <kyrylogolovkov@gmail.com>
2022-09-12 17:24:25 +0300

    HW8

--- Month-1/Students/Golovkov/Lesson-8/basic.js
+++ Month-1/Students/Golovkov/Lesson-8/basic.js
@@ -1,10 +1,29 @@
-/*
-Устанавливаем cypress:
-npm install cypress
-*/
+/* Сформировать селекторы на форму отправки комментария к посту
+emoji
+Фото
+GIF
+video
+textArea
+cancel
+publish
 
-/*
-Устанавливаем проверочные тесты и запускаем cypress:
-npx cypress open
-*/
 
+button[aria-label="Add an emoji"]  - emoji
+input[role=button]          -    фото
+[aria-label='Add a GIF']     -   GIF
+button[aria-label$=video]    -   video
+#placeholder-editor    -      textArea
+[data-hook=cancel-button]    -  cancel
+[aria-disabled]:not([aria-expanded])  -  publish
+
+
+Home task:
+
+Используя Xpath построить селекторы для формы обратной связи
+
+//input[@id="input_comp-kdr40wmu1"]   - first name
+//input[@name="last-name"]           - last name
+//*[@id="comp-kdr40wnd"] //input     - e-mail
+//*[@class="_1VWbH has-custom-focus"] - message
+//*[@class='_3wqEk']                  - Submit
+*/
